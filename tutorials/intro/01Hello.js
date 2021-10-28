8c1b7e97d8a8aeefa5356fc117cadc46896a1b03
Ronald.Barrera <ronald.barrera@gizlocorp.com>
2021-10-27 22:55:14 -0500

    dia 1

---
+++ tutorials/intro/01Hello.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Tex, View} from 'react-native';
+
+const YourApp = (){
+    return (
+        <View style={{ flex:1, justifyContent:"center", alignItems:"center" }}>
+
+            <Text>
+Hello!!
+
+            </Text>
+        </View>
+
+    );
+}
+export default YourApp;
