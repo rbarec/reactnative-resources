2efa1bece3886c05b64dd1fb3f8807121defe8a2
Ronald.Barrera <ronald.barrera@gizlocorp.com>
2021-10-27 23:14:53 -0500

    func class

---
+++ tutorials/03Hello_byclass.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import { View, Text } from 'react-native';
+
+class MyApp3 extends Component{
+    render(){
+        return (
+            <View><Text>Hola soy una clase ext.Compoentn de REact</Text></View>
+        );
+    }
+}
+
+export default MyApp3;
---
+++ tutorials/intro/02Hello_byfunction.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ViewBase, Text, View } from 'react-native';
+
+const MyApp2 = () => {
+return (
+    <View>
+        <Text>
+    Hola soy un function component
+
+        </Text>
+    </View>
+
+);
+
+}
+
+export default MyApp2;
